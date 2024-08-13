ab2562b3e47f98492fb32c019a99bbbc24176c69
guxiaobai <sikuan.gu@gmail.com>
2024-08-13 23:01:16 +0800

    自动安装

---
+++ 002-ventoy/inject/VentoyAutoRun.bat
@@ -0,0 +1,27 @@
+@echo off
+
+REM ###########################################################################
+REM  This is an example of Ventoy windows injection auto run batch script
+REM
+REM  The 1st parameter is the ISO file full path. 
+REM     For example: C:\ISO\Windows11.iso
+REM  
+REM  The 2nd parameter is drive letter of the ISO file mountpoint.
+REM     For example: F
+REM 
+REM  The output of this script will be saved to X:\VentoyAutoRun.log
+REM 
+REM ###########################################################################
+
+set ISOFILE=%1
+set MNTPOINT=%2
+
+diskpart /s X:\CreatePartitions-UEFI-FFU.txt
+
+REM ###########################################################################
+REM  Do your main process from here
+REM ###########################################################################
+
+echo ISOFILE=%ISOFILE%
+echo MNTPOINT=%MNTPOINT%
+echo This is an example of Ventoy Windows Injection Auto Run
